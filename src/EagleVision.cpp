5cf9f0289c55791eb3640557c00f563d4eab0801
snekiam <snekiam@gmail.com>
2016-07-05 04:19:58 +0000

    removed unused angle normalization misc. changes to remove UI elements for running in console now writes an image to disk on startup (goals.jpg)

--- src/EagleVision.cpp
+++ src/EagleVision.cpp
@@ -16,9 +16,12 @@ int main()
 	int i = 0;
 	cv::VideoCapture cap("http://10.0.1.13:8080/?action=stream");
 	cv::Mat image;
-	cv::Mat goalImage = cv::imread("goal.jpg", CV_LOAD_IMAGE_COLOR);
-
+	cv::Mat goalImage = cv::imread("./goal.jpg", CV_LOAD_IMAGE_COLOR);
+	if(cap.isOpened()){
+		cout<<"opened cap";
+	}
 	cap.read(image);
+	imwrite("./goals.jpg",image);
 	Mat thresh, binary, hsv, contour_mat;
 	vector<vector<Point> > contours;
 	vector<vector<Point> > goal;
@@ -34,11 +37,11 @@ int main()
     									 cv::Size(2 * 2 + 1, 2 * 2 + 1),
 										 cv::Point(2, 2) );
 	cv::cvtColor( goalImage, goalImage, COLOR_BGR2HSV );//convert to HSV
-	cv::inRange( goalImage, Scalar( 56, 202, 23 ), Scalar( 71, 255, 206 ), goalImage );//threshold the HSV colortype
+	cv::inRange( goalImage, Scalar( 56, 202, 23 ), Scalar( 81, 255, 206 ), goalImage );//threshold the HSV colortype
 	cv::dilate( goalImage , goalImage, element );
 	cv::erode( goalImage , goalImage, element ); //dilate then erode to close any irregularities
-    cv::namedWindow("test",CV_WINDOW_AUTOSIZE);
-    cv::imshow("test",goalImage);
+//    cv::namedWindow("test",CV_WINDOW_AUTOSIZE);
+//    cv::imshow("test",goalImage);
     Scalar color = Scalar( 0,255,0 );
 
     cv::findContours(goalImage,goal,hierarchy, cv::RETR_TREE,cv::CHAIN_APPROX_SIMPLE);
@@ -47,15 +50,15 @@ int main()
 		{
 		 	cout<<"Bad input image!";
 		}
-		cv::namedWindow( "Cam", CV_WINDOW_AUTOSIZE );// create a window for the og image.
-		cv::imshow( "Cam", image );// show our image inside it.
-		cv::namedWindow( "Thresholded", CV_WINDOW_AUTOSIZE );//create a window for the thresholded image
+//		cv::namedWindow( "Cam", CV_WINDOW_AUTOSIZE );// create a window for the og image.
+//		cv::imshow( "Cam", image );// show our image inside it.
+//		cv::namedWindow( "Thresholded", CV_WINDOW_AUTOSIZE );//create a window for the thresholded image
 		cv::cvtColor( image, hsv, COLOR_BGR2HSV );//convert to HSV
-		cv::waitKey( 16.67 );//wait 16.67 ms (30fps)
+//		cv::waitKey( 16.67 );//wait 16.67 ms (30fps)
 		cv::inRange( hsv, Scalar( 56, 202, 23 ), Scalar( 71, 255, 206 ), thresh );//threshold the HSV colortype
 		cv::dilate( thresh , thresh, element );
 		cv::erode( thresh , thresh, element ); //dilate then erode to close any irregularities
-		cv::imshow( "Thresholded", thresh );
+//		cv::imshow( "Thresholded", thresh );
 		cv::findContours(thresh,contours,hierarchy, cv::RETR_TREE,cv::CHAIN_APPROX_SIMPLE);
 		Mat goalDrawing = Mat::zeros( thresh.size(), CV_8UC3 );
 		Mat drawing = Mat::zeros( thresh.size(), CV_8UC3 );
@@ -71,8 +74,8 @@ int main()
 	           	drawContours(goalDrawing,contours,contourOfInterest,color,2,8,hierarchy,0,Point());
 				Rect rect = boundingRect(contours[contourOfInterest]);
 				rectangle(goalDrawing, rect.tl(), rect.br(), cv::Scalar(100, 100, 200), 2, CV_AA);
-	            cv::namedWindow("selected contour",CV_WINDOW_AUTOSIZE);
-	            imshow("selected contour",goalDrawing);
+//	            cv::namedWindow("selected contour",CV_WINDOW_AUTOSIZE);
+//	            imshow("selected contour",goalDrawing);
 	        }
 	        else{
 	        	contourOfInterest = -1;
@@ -81,30 +84,21 @@ int main()
 	        drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
 	    }
 	    Point centerPoint;
-	    cv::namedWindow("contours",CV_WINDOW_AUTOSIZE);
-		cv::imshow( "contours", drawing);
+//	    cv::namedWindow("contours",CV_WINDOW_AUTOSIZE);
+//		cv::imshow( "contours", drawing);
+		cout<<"\nContour of Interest: "<<contourOfInterest<<"\n";
 		if(contourOfInterest != -1){
 			Rect rect = boundingRect(contours[contourOfInterest]);
-			distance = (4.4 * 14 * 240)/(rect.height * 3);//focal length (mm) * height (inches) * photo hieght(pixels)/(obj height(pixels)*vertical sensor size(mm))
+			distance = (4.4 * 14 * 480)/(rect.height * 3);//focal length (mm) * height (inches) * photo hieght(pixels)/(obj height(pixels)*vertical sensor size(mm))
 			cout <<"dist"<<distance<<"\n";
 			centerPoint = Point((rect.x + rect.width/2), (rect.y + rect.height/2));
-			float imageCntr = 432/2-.5;
+			float imageCntr = 640/2-.5;
 			cout << "center point " <<centerPoint.x<<","<<centerPoint.y;
-			focalLength = (0.5 * 432 / tan(74/2));
+			focalLength = (0.5 * 640 / tan(74/2));
 			angle = atan( ( centerPoint.x - imageCntr ) / focalLength);
 			cout<<"azimuth " <<angle * 180 / 3.14;
 		}
 		cap.read( image );
 	}
     return 0;
-}
-
-double normalizeAngle(double angle){
-	while(angle >= 360.0){
-		angle -=360;
-	}
-	while(angle <0.0){
-		angle +=360.0;
-	}
-	return angle;
 }
